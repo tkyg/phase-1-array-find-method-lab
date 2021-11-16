245ed89a1b6f37c15911d99429f1131bd1f64636
tkyg <80114433+tkyg@users.noreply.github.com>
2021-11-15 21:45:47 -0500

    commit tree 36623abafb94ee7d980864036824c3e90e96ced7

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(record) {
+    let winningYear = record.find((record) => record.result ==='W')
+    if (winningYear) {
+      return winningYear.year;
+    }
+  }
+  superbowlWin(record);
